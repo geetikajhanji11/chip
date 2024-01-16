3eb9b6e8af0090e4d46e3d423b9a8bf7085d8c5b
Geetika Jhanji <geetikajhanji11@gmail.com>
2024-01-16 23:38:01 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   useEffect(() => {
     const fetchRandomUsers = async () => {
       try {
-        const response = await fetch('https://randomuser.me/api/?results=10');
+        const response = await fetch('https://randomuser.me/api/?results=20&nat=us');
         
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
